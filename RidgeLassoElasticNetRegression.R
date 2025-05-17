32d219c2455643a8b48cb1b08700d5afe81ddeda
bestEff0rts <dfoundation@mail.ru>
2025-05-17 20:40:44 +0300

    Update RidgeLassoElasticNetRegression.R

--- RidgeLassoElasticNetRegression.R
+++ RidgeLassoElasticNetRegression.R
@@ -62,3 +62,103 @@ mseplot <- ggplot(results, aes(x=alpha, y=mse)) +
 
 ggplotly(mseplot) # MSE по alpha
 
+library(plotly)
+lambda_values <- c(0, 10, 20, 40)
+slope_range <- seq(-2, 2, length.out = 500)
+ssr <- 10  # Фиксированное значение SSR
+
+#расчет данных для Ridge penalty
+ridge_data <- data.frame(
+  slope = rep(slope_range, times = length(lambda_values)),
+  lambda = factor(rep(lambda_values, each = length(slope_range)), 
+                  levels = c("40", "20", "10", "0")),
+  penalty = ssr + rep(lambda_values, each = length(slope_range)) * 
+    rep(slope_range, times = length(lambda_values))^2
+)
+
+#интерактивный график
+plotridge <- plot_ly() %>%
+  add_lines(
+    data = subset(ridge_data, lambda == "40"),
+    x = ~slope, y = ~penalty,
+    name = "λ=40", line = list(color = "red", width = 2),
+    hoverinfo = "text",
+    text = ~paste("λ=40<br>β:", round(slope, 2), "<br>Penalty:", round(penalty, 2))
+  ) %>%
+  add_lines(
+    data = subset(ridge_data, lambda == "20"),
+    x = ~slope, y = ~penalty,
+    name = "λ=20", line = list(color = "green", width = 2),
+    hoverinfo = "text",
+    text = ~paste("λ=20<br>β:", round(slope, 2), "<br>Penalty:", round(penalty, 2))
+  ) %>%
+  add_lines(
+    data = subset(ridge_data, lambda == "10"),
+    x = ~slope, y = ~penalty,
+    name = "λ=10", line = list(color = "blue", width = 2),
+    hoverinfo = "text",
+    text = ~paste("λ=10<br>β:", round(slope, 2), "<br>Penalty:", round(penalty, 2))
+  ) %>%
+  add_lines(
+    data = subset(ridge_data, lambda == "0"),
+    x = ~slope, y = ~penalty,
+    name = "λ=0", line = list(color = "black", width = 2),
+    hoverinfo = "text",
+    text = ~paste("λ=0<br>β:", round(slope, 2), "<br>Penalty:", round(penalty, 2))
+  ) %>%
+  layout(
+    title = list(
+      text = "<b>Ridge Penalty: SSR + λ×β²</b>",
+      x = 0.5,
+      font = list(size = 16)
+    ),
+    xaxis = list(
+      title = "Slope Values (β)",
+      range = c(-2, 2),
+      tickvals = seq(-2, 2, 1),
+      zeroline = FALSE,
+      showgrid = TRUE,
+      gridcolor = "lightgray"
+    ),
+    yaxis = list(
+      title = "Penalty Value",
+      range = c(0, max(ridge_data$penalty) + 5),
+      showgrid = TRUE,
+      gridcolor = "lightgray"
+    ),
+    legend = list(
+      orientation = "h",
+      x = 0.5,
+      y = 1.1,
+      xanchor = "center"
+    ),
+    hovermode = "x unified",
+    shapes = list(
+      list(
+        type = "line",
+        x0 = 0,
+        x1 = 0,
+        y0 = 0,
+        y1 = max(ridge_data$penalty),
+        line = list(color = "gray", dash = "dash")
+      )
+    ),
+    annotations = list(
+      list(
+        x = 1.5,
+        y = max(ridge_data$penalty) * 0.9,
+        text = "Парабола сдвигается вверх и сужается с ростом λ",
+        showarrow = FALSE,
+        font = list(size = 14)
+      ),
+      list(
+        x = 0,
+        y = ssr,
+        text = "SSR",
+        showarrow = FALSE,
+        font = list(size = 14),
+        yshift = 10
+      )
+    )
+  )
+plotridge
